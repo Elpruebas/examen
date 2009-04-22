97e7a61ac1e5c5d89c0e007977be07d4eb3da9c2
mio <mio@mio.es>
2009-04-22 09:43:12 +0200

    cambio

--- peliculas-view.tpl.php
+++ peliculas-view.tpl.php
@@ -1,5 +1,5 @@
 <?php if(empty($peliculas)):
-        print "No hay peliculas en estos momentos.";
+        print "No hasdfasdfasdfasdsdfasdfasdfasdfasdfasdfy peliculas en estos momentos.";
       else: ?>
     <?php foreach($peliculas as $pelicula): ?>
         <?php print pelicula_view($pelicula, true, false)->content['pelicula']['#value'];?>
